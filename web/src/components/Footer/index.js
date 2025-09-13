1010760b315da9d7cb1aea766c8e546880b15e0b
Raick Staine <r.staine@aluno.ifsp.edu.br>
2025-09-13 15:31:52 -0300

    Footer finalizado

--- web/src/components/Footer/index.js
+++ web/src/components/Footer/index.js
@@ -1,30 +1,31 @@
-import Image from "next/image";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div className={styles.container}>
-
-                <div className={styles.logo}>NEOBYTE</div>
-
-                <div className={styles.links}>
-                    <span className={styles.sac}>SAC: 3888-8899</span>
-                    <a href="/sobre" className={styles.link}>
-                        Sobre nós
-                    </a>
+            <div className={styles.contentWrapper}>
+                <div className={styles.leftBox}>
+                    <div className={styles.logo}>
+                        <h1>NEOBYTE</h1>
+                    </div>
                 </div>
-
-                <div className={styles.social}>
-                    <img src="/Neobyte/facebook.svg" alt="Perfil" />
-                    <img src="/Neobyte/instagram.svg" alt="Favorito" />
-                    <img src="/Neobyte/twitter.svg" alt="Carrinho" />
+                <div className={styles.rightBox}>
+                    <div className={styles.topRow}>
+                        <div className={styles.centerContent}>
+                            <span className={styles.sac}>SAC: 3888-8899</span>
+                            <a href="/sobre" className={styles.link}>Sobre nós</a>
+                        </div>
+                        <div className={styles.icons}>
+                            <img src="/Neobyte/facebook.svg" alt="Facebook" />
+                            <img src="/Neobyte/instagram.svg" alt="Instagram" />
+                            <img src="/Neobyte/twitter.svg" alt="Twitter" />
+                        </div>
+                    </div>
+                    <div className={styles.line}></div>
+                    <div className={styles.copy}>
+                        © 2025 Todos os direitos garantidos por Neobyte
+                    </div>
                 </div>
-            </div>
-
-            {/* Linha divisória */}
-            <div className={styles.copy}>
-                © 2025 Todos os direitos garantidos por Neobyte
             </div>
         </footer>
     );
