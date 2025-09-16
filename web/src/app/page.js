a489f9a1fe96723f632b0e2cadff77ba29dfe3ad
Weslley Souza <souza.weslley@aluno.ifsp.edu.br>
2025-09-16 13:30:48 -0300

    Iniciando a criacao dos cards

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,9 +1,31 @@
 import styles from "./page.module.css";
+import Head from "next/head";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import Produtos from "@/components/Produtos";
 import Footer from "@/components/Footer";
 
+
 export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Neobyte</title>
+        <meta name="description" content="O melhor e-commerce tecnológico do mercado" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="./favicon.svg" />
+      </Head>
+      <Header/>
+      <section>
+        <Banner/>
+        <Produtos/>
+      </section>
+      <Footer/>
+    </>
+  );
+}
+
+/* export default function Home() {
   return (
     <div>
       <section>
@@ -15,8 +37,12 @@ export default function Home() {
       </section>
 
       <section>
+        <Produtos />
+      </section>
+
+      <section>
         <Footer />
       </section>
     </div>
   );
-}
+} */
